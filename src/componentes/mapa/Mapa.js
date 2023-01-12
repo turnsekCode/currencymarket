86209ec3268fc854a7b864ce89763f52689a1e11
abraham <abraham.turnsek@gmail.com>
2023-01-12 12:49:40 +0100

    ultima actualizacion del mapa funcionando

--- src/componentes/mapa/Mapa.js
+++ src/componentes/mapa/Mapa.js
@@ -29,6 +29,42 @@ const Mapa = () => {
             <p>Albufera</p>
             <img className="market_img" src="../market.png" alt="market" />
           </Marker>
+          <Marker longitude={-3.6461429} latitude={40.4341911}>
+            <p>Quintana</p>
+            <img className="market_img" src="../market.png" alt="market" />
+          </Marker>
+          <Marker longitude={-3.6951906} latitude={40.4011045}>
+            <p>Delicias</p>
+            <img className="market_img" src="../market.png" alt="market" />
+          </Marker>
+          <Marker longitude={-3.712925499999999} latitude={40.3962949}>
+            <p>Usera</p>
+            <img className="market_img" src="../market.png" alt="market" />
+          </Marker>
+          <Marker longitude={-3.7039902999999867} latitude={40.4488839}>
+            <p>Cuatro Caminos</p>
+            <img className="market_img" src="../market.png" alt="market" />
+          </Marker>
+          <Marker longitude={-3.7073656999999685} latitude={40.4232377}>
+            <p>San Bernardo</p>
+            <img className="market_img" src="../market.png" alt="market" />
+          </Marker>
+          <Marker longitude={-3.705431599999997} latitude={40.4121929}>
+            <p>Tirso de Molina</p>
+            <img className="market_img" src="../market.png" alt="market" />
+          </Marker>
+          <Marker longitude={-3.713297099999977} latitude={40.4351005}>
+            <p>Guzman el Bueno</p>
+            <img className="market_img" src="../market.png" alt="market" />
+          </Marker>
+          <Marker longitude={-3.698444600000016} latitude={40.4603827}>
+            <p>Tetuan</p>
+            <img className="market_img" src="../market.png" alt="market" />
+          </Marker>
+          <Marker longitude={-3.665292000000022} latitude={40.429553}>
+            <p>Alcala</p>
+            <img className="market_img" src="../market.png" alt="market" />
+          </Marker>
         </Map>
       </div>
     </section>
